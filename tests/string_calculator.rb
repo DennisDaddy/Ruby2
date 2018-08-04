bd6b1b4c580c7bc28035e5d2a8ec248d00901f62
DennisDaddy <softteez95@gmail.com>
2018-08-04 17:30:21 +0300

    Test sum of numbers

---
+++ tests/string_calculator.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+
+class Calculator
+    def initialize(digits_string)
+        @digits = digits_string.split(',').map { |n| n.to_i}
+    end
+
+    def sum
+        @digits.inject(:+)
+    end
+end
+
+describe Calculator do
+    it 'sums provided digits' do
+        calc = Calculator.new('1,2,3,4,5')
+        calc.sum.must_equal(15)
+    end
+end
