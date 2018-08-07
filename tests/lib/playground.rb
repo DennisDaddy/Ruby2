6cde6ae17d8859c24c71dfd845f659ce99946503
DennisDaddy <softteez95@gmail.com>
2018-08-07 08:08:35 +0300

    Implement first test

---
+++ tests/lib/playground.rb
@@ -0,0 +1,10 @@
+class Playground
+	def initialize(children)
+		
+	end
+
+	def mood
+		'boring'		
+	end
+	
+end
---
+++ tests/spec/playground_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/playground'
+describe Playground do
+	context 'when there are no children' do
+		it 'is quite boring place' do
+			playground = Playground.new(0)
+			mood = playground.mood
+			expect(mood).to eq('boring')
+		end
+
+		it 'is empty'
+	end
+end
